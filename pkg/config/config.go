430593f9ba6aa99730fbae9760d8ed8663a8c206
David Ouellette <david.o@mailbox.org>
2024-08-30 16:05:43 -0700

    Set the default model to gpt-4-turbo.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -11,7 +11,7 @@ type Config struct {
 	ApiKey       string `split_words:"true" validate:"required"`
 	LogLevel     string `split_words:"true" default:"INFO" validate:"required,oneof=TRACE DEBUG INFO WARN ERROR FATAL"`
 	ProjectRoot  string `split_words:"true" validate:"required"`
-	ModelVersion string `split_words:"true" default:"gpt-4o" validate:"required,oneof=gpt-4o"`
+	ModelVersion string `split_words:"true" default:"gpt-4-turbo" validate:"required,oneof=gpt-4o gpt-4-turbo"`
 }
 
 func MustProcessConfig() *Config {

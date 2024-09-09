669127cb89e2bc0395990310ab53b9347d437ef0
David Ouellette <david.o@mailbox.org>
2024-09-08 17:40:54 -0700

    Use gpt-4o as the default.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,5 +2,5 @@ package config
 
 type Config struct {
 	ApiKey       string `config_format:"snake" validate:"required"`
-	ModelVersion string `config_format:"snake" config_default:"gpt-4-turbo" validate:"required,oneof=gpt-4o gpt-4-turbo"`
+	ModelVersion string `config_format:"snake" config_default:"gpt-4o" validate:"required,oneof=gpt-4o gpt-4-turbo"`
 }
